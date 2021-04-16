422facae5c017e9b28dd5264ad5e039e9880c886
sunghyunjeonme <4dallove@gmail.com>
2021-04-16 22:29:11 +0900

    몽구스 설정

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://sunghyunjeonme:yu5282yu@practice-node-react.9ofkp.mongodb.net/test",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
