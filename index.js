bda6bb48aebdfc1fc9f0a4e84e3e21b69ea25695
sunghyunjeonme <4dallove@gmail.com>
2021-04-20 18:21:39 +0900

    Add : 유저 모델 및 스키마 생성

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://sunghyunjeonme:yu5282yu@practice-node-react.9ofkp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
