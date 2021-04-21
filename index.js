fd1a8b7cd359128020e7e5aef58cb9da827fdb48
sunghyunjeonme <4dallove@gmail.com>
2021-04-21 23:25:52 +0900

    Add : 노드몬 추가

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded 데이터를 분석해서 가져옴
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json을 분석해줌
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 mongoose
   .connect(
@@ -16,9 +24,20 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World");
 });
 
+// [회원가입을 위한 Route]
+app.post("/register", (req, res) => {
+  // 회원가입할 때 필요한 정보를 클라이언트에서 가지고 오면
+  // 그것을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+  // 정보들이 유저 모델에 저장된다.
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
